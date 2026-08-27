929d3786808b1ec3750dc7c4c078ef5377548ccf
FANGNON Mawouégnigan Grégoire <fmgexcellence@gmail.com>
2026-08-27 15:08:56 +0100

    Correction admin.py courses : enregistrement des modeles dans l'interface admin

--- backend/courses/admin.py
+++ backend/courses/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import DomaineEtude, Parcours, Cours, Lecon
 
-# Register your models here.
+
+@admin.register(DomaineEtude)
+class DomaineEtudeAdmin(admin.ModelAdmin):
+    list_display = ("nom", "date_creation")
+    search_fields = ("nom",)
+
+
+@admin.register(Parcours)
+class ParcoursAdmin(admin.ModelAdmin):
+    list_display = ("nom", "domaine", "duree_estimee_heures")
+    list_filter = ("domaine",)
+    search_fields = ("nom",)
+
+
+@admin.register(Cours)
+class CoursAdmin(admin.ModelAdmin):
+    list_display = ("titre", "parcours", "ordre")
+    list_filter = ("parcours",)
+
+
+@admin.register(Lecon)
+class LeconAdmin(admin.ModelAdmin):
+    list_display = ("titre", "cours", "ordre")
+    list_filter = ("cours",)
